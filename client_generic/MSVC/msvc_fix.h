1e81099aeb43f335187b7ae3a0b30035ca65e8f5
rcplug@gmail.com <rcplug@gmail.com@bfcdfc66-ee7c-11de-a004-2f18c49ebcc3>
2011-03-22 20:46:10 +0000

    [Windows only] x64 and ffmpeg compilation issues solved, settings gui x64 project file

--- client_generic/MSVC/msvc_fix.h
+++ client_generic/MSVC/msvc_fix.h
@@ -1,8 +1,28 @@
 #pragma once
 #include <direct.h>
 #include <string>
+#include <time.h>
+#include <Windows.h>
 
-int snprintf(char *buffer, size_t count, const char *fmt, ...);
+extern "C" 
+{ 
+
+	int snprintf(char *buffer, size_t count, const char *fmt, ...);
+#ifdef _WIN64
+	void ___chkstk();
+	void usleep(int ms);
+	double __strtod(const char *_Str, char **_EndPtr);
+
+	struct timezone 
+	{
+		int  tz_minuteswest; /* minutes W of Greenwich */
+		int  tz_dsttime;     /* type of dst correction */
+	};
+
+	int gettimeofday(struct timeval *tv, struct timezone *tz);
+#endif
+
+};
 
 //#ifdef _DEBUG
 //#include <crtdbg.h>

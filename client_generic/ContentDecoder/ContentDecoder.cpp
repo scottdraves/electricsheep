scottdraves/electricsheep

commit 08d1d376cf7dbe7b6345e3acbfb4df0402452893

replace av_free_packet with av_packet_unref

client_generic/ContentDecoder/ContentDecoder.cpp
@@ -535,7 +535,7 @@ CVideoFrame *CContentDecoder::ReadOneFrame(sOpenVideoInfo *ovi)
 			if ( av_read_frame( pFormatContext, &packet ) < 0 )
 			{
 				ovi->m_ReadingTrailingFrames = true;
-				av_free_packet(&packet);
+				av_packet_unref(&packet);
 				continue;
 			}
 		}
@@ -567,7 +567,7 @@ CVideoFrame *CContentDecoder::ReadOneFrame(sOpenVideoInfo *ovi)
             break;
         }
         
-		av_free_packet(&packet);
+		av_packet_unref(&packet);
     }
 
     //	Do we have a fresh frame?
@@ -622,7 +622,7 @@ CVideoFrame *CContentDecoder::ReadOneFrame(sOpenVideoInfo *ovi)
         ovi->m_NextIsSeam = false;
     }
 
-    av_free_packet( &packet );
+    av_packet_unref( &packet );
     return pVideoFrame;
 }
 

commit 00919a568a8b51b59f13eb196b7efe86edd28573

fix memory leak (introduced in 08d1d376)

client_generic/ContentDecoder/ContentDecoder.cpp
@@ -560,7 +560,7 @@ CVideoFrame *CContentDecoder::ReadOneFrame(sOpenVideoInfo *ovi)
         if ( av_read_frame( pFormatContext, packet ) < 0 )
           {
             ovi->m_ReadingTrailingFrames = true;
-            av_packet_unref(packet);
+            av_packet_free(&packet);
             continue;
           }
             
@@ -576,7 +576,7 @@ CVideoFrame *CContentDecoder::ReadOneFrame(sOpenVideoInfo *ovi)
             break;
           }
         
-        av_packet_unref(packet);
+        av_packet_free(&packet);
     }
 
     //	Do we have a fresh frame?
@@ -630,7 +630,7 @@ CVideoFrame *CContentDecoder::ReadOneFrame(sOpenVideoInfo *ovi)
         ovi->m_NextIsSeam = false;
     }
 
-    av_packet_unref( packet );
+    av_packet_free(&packet);
     return pVideoFrame;
 }
 